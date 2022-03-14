421749b0a6469940dff581452869ebd27b1e4499
aditikuls4 <kulshresthaaditi9@gmail.com>
2022-03-14 12:23:31 +0530

    commit

---
+++ AuthorizationMicroservice/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user (userid,upassword,uname) values ('satyam','satyam','satyam');
+insert into user (userid,upassword,uname) values ('moksh','moksh','moksh');
+insert into user (userid,upassword,uname) values ('shivani','shivani','shivani');
+insert into user (userid,upassword,uname) values ('aditi','aditi','aditi');
+insert into user (userid,upassword,uname) values ('piyush','piyush','piyush');
